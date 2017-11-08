e775cef3cfafe9b6a56e2a3c3df86c156011b7d6
Michael Haines <mhaines333@gmail.com>
2017-11-07 22:59:36 -0500

    Code is mostly working

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "Servo.h"
+#include "math.h"
+
+#define NUMBER_OF_SAMPLES 50
 
  int main(){
     init();
@@ -20,16 +23,16 @@
     return 0;
 }
 
-void ping_rising();
-void ping_falling();
-
-uint16_t calculateDistance();
-const uint8_t trigPin = 7;
-const uint8_t echoPin = 2;
-// Variables for the duration and the distance
+const uint8_t trigPin = 8;
+const uint8_t echoPin = 7;
+uint32_t last_servo = 0;
+uint8_t rotation = 0;
+uint8_t reverse = 0;
+uint8_t counter = 0;
+uint32_t distance_array[NUMBER_OF_SAMPLES];
+volatile uint32_t distance;
 volatile uint32_t int_duration;
 volatile uint32_t duration;
-volatile uint32_t distance;
 volatile uint8_t still_pulsed;
 
 int16_t baseAngle = 0;
@@ -41,56 +44,18 @@ void setup() {
     Serial.begin(115200);
     baseServo.attach(9); // Defines on which pin is the servo motor attached
     topServo.attach(11, -90, 90);
+    topServo.write(0);
 
-    //DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
-
-    //attachInterrupt(interrupt, ISR, mode) PD2 is now an input with pull-up enabled
-    //attachInterrupt(digitalPinToInterrupt(2), ping_rising, CHANGE);
-    //attachInterrupt(1, ping_falling, FALLING);
+    pinMode(echoPin, INPUT);
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 
     cli();
-    EICRA |= (1 << ISC00);
-    EIMSK |= (1 << INT0);     // Turns on INT0
+    PCICR |= (1 << PCIE2);
+    PCMSK2 |= (1 << PCINT23);
     sei();
-    pinMode(3, INPUT);
-    pinMode(echoPin, INPUT_PULLUP);
-    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 }
-void loop() {
 
-    // rotates the servo motor from 15 to 165 degrees
-    for(int i=15;i<=165;i++){
-        baseServo.write(i);
-        //delay(30);
-        distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
-
-        //Serial.print(i); // Sends the current degree into the Serial Port
-        //Serial.print(", "); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-        //Serial.print(distance); // Sends the distance value into the Serial Port
-        //Serial.println("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-    }
-    delay(1000);
-    if(baseAngle ==90){
-        baseAngle=0;
-    }
-
-    baseAngle+=5;
-    //Serial.println(baseAngle);
-    topServo.write(baseAngle);
-
-    // Repeats the previous lines from 165 to 15 degrees
-    for(int i=165;i>15;i--){
-        baseServo.write(i);
-        //delay(30);
-        distance = calculateDistance();
-        //Serial.print(i);
-        //Serial.print(", ");
-        //Serial.print(distance);
-        //Serial.println(".");
-    }
-}
-// Function for calculating the distance measured by the Ultrasonic sensor
-uint16_t calculateDistance(){
+void calculateDistance(){
 
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
@@ -101,27 +66,76 @@ uint16_t calculateDistance(){
     digitalWrite(trigPin, LOW);
 }
 
-void ping_rising(){
-    if (!(PIND && PD2))
-        Serial.println(PIND & PD2);
-    int_duration = millis();
+// Need for this function varies
+static int32_t uint32Compare(const void * a,const void * b)
+{
+    if (*(uint32_t*)a==*(uint32_t*)b)
+        return 0;
+    else if (*(uint32_t*)a < *(uint32_t*)b)
+        return -1;
+    else
+        return 1;
 }
 
-ISR(INT0_vect){
-    int value = PIND & PD2;
+void loop() {
+
+    if (20 < millis() - last_servo) {
+        last_servo = (uint32_t) millis();
+        baseServo.write(rotation);
+        if (reverse)
+            rotation--;
+        else
+            rotation++;
+    }
+
+    if (rotation == 140 && reverse == 0) {
+        reverse = 1;
+        if(baseAngle==90)
+            baseAngle=0;
+        baseAngle+=5;
+        topServo.write(baseAngle);
+
+    }
+    else if (rotation == 0 && reverse == 1){
+        reverse = 0;
+    }
+
+    if (!still_pulsed)
+        //counter++;
+        //distance_array[counter%=NUMBER_OF_SAMPLES] = duration *0.034/2;
+        calculateDistance();
+
+    //uint32_t tmp[NUMBER_OF_SAMPLES];
+    //memcpy(&tmp[0], &distance_array[0] , NUMBER_OF_SAMPLES*sizeof(uint32_t));
+    //qsort(tmp, NUMBER_OF_SAMPLES, sizeof(uint32_t), uint32Compare);
+    // distance = tmp[NUMBER_OF_SAMPLES/2]
+    if ((millis()%120) == 0) {
+        Serial.println(distance);
+        uint16_t distance_x = (uint16_t) (distance * cos(baseAngle * PI / 180));
+        uint16_t distance_y = (uint16_t) (distance * sin(baseAngle * PI / 180));
+        Serial.print("x, y distance: ");
+        Serial.print(distance_x);
+        Serial.print(", ");
+        Serial.print(distance_y);
+        Serial.print(", ");
+        Serial.println(baseAngle);
+    }
+}
+
+
+ISR(PCINT2_vect){
+    int value = PIND & (1 << PINB7);
     //duration = pulseIn(echoPin, LOW); // Reads the echoPin, returns the sound wave travel time in microseconds
     //distance= duration*0.034/2;
-    if ((value == 0) && !still_pulsed){
+    if ((value > 0) && !still_pulsed){
         //Serial.println("test1");
-        int_duration = millis();
+        int_duration = (uint32_t) micros();
         still_pulsed = 1;
     }
-    else if ((value > 0) && still_pulsed){
-        duration = millis() - int_duration;
+    else if ((value == 0) && still_pulsed){
+        duration = (uint32_t) (micros() - int_duration);
         still_pulsed = 0;
-        distance = duration *0.034/2;
-        //Serial.println("Winner!");
-        //Serial.println(distance);
+        distance = (uint32_t) (duration * 0.034 / 2);
     }
-    Serial.print(PIND & 0x03);
+    //Serial.println(PIND & (1 << PINB7));
 }
