1ee050a5ce377511a7a29898e4bde8f06d8a2c73
Michael Haines <mphaines88@gmail.com>
2017-11-09 20:09:38 -0500

    Fixed some Geometry

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "Arduino.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "Servo.h"
-#include "math.h"
+#include "Servo.h" //For controlling the servoe
+#include "math.h" //For calculating angles
 
-#define NUMBER_OF_SAMPLES 50
+#define NUMBER_OF_SAMPLES 50 //will be used later, could be any number within bounds, really
 
  int main(){
     init();
@@ -23,34 +23,35 @@
     return 0;
 }
 
-const uint8_t trigPin = 8;
+const uint8_t trigPin = 8; //set pins and starting points
 const uint8_t echoPin = 7;
-uint32_t last_servo = 0;
-uint8_t rotation = 0;
-uint8_t reverse = 0;
-uint8_t counter = 0;
+uint32_t last_servo = 0; //the last time the base servo was moved
+uint8_t rotation = 20; //The base servo angle will be called rotation and start at 20 degrees, yaw
+uint8_t reverse = 0; //sweeping backwards?
+uint8_t counter = 0; //used to fill distance array
 uint32_t distance_array[NUMBER_OF_SAMPLES];
 volatile uint32_t distance;
 volatile uint32_t int_duration;
 volatile uint32_t duration;
 volatile uint8_t still_pulsed;
+int16_t distance_old = 0;
 
-int16_t baseAngle = 0;
-Servo baseServo;
-Servo topServo;// Creates a servo object for controlling the servo motor
+int16_t topAngle = 50; //angle corresponding to topServo (yeah, not the best naming convetion), pitch/roll
+Servo baseServo; //Creates a servo object for controlling the servo motor
+Servo topServo;
 
 void setup() {
-    //pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    //pinMode(echoPin, INPUT); //Sets the echoPin as an Input
     Serial.begin(115200);
-    baseServo.attach(9); // Defines on which pin is the servo motor attached
-    topServo.attach(11, -90, 90);
-    topServo.write(0);
-
+    baseServo.attach(9); //Defines on which pin is the servo motor attached
+    topServo.attach(11); //pin 11, goes between 90 and -90 degrees
+    baseServo.write(topAngle); //Set initial position to 0, sensor is flat with the horizontal, speakers facing straight down
+    //delay(50000);
     pinMode(echoPin, INPUT);
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 
-    cli();
-    PCICR |= (1 << PCIE2);
+    cli(); //external interrupt
+    PCICR |= (1 << PCIE2); //activates interrupt if pin 7 goes changes state
     PCMSK2 |= (1 << PCINT23);
     sei();
 }
@@ -59,16 +60,15 @@ void calculateDistance(){
 
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
+    
     // Sets the trigPin on HIGH state for 10 micro seconds
-
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
 }
 
 // Need for this function varies
-static int32_t uint32Compare(const void * a,const void * b)
-{
+static int32_t uint32Compare(const void * a,const void * b){//used with qsort; none of us really understand what this part does
     if (*(uint32_t*)a==*(uint32_t*)b)
         return 0;
     else if (*(uint32_t*)a < *(uint32_t*)b)
@@ -79,64 +79,66 @@ static int32_t uint32Compare(const void * a,const void * b)
 
 void loop() {
 
-    if (20 < millis() - last_servo) {
+    if (45 < millis() - last_servo) { //Move every 20 (or more) milliseconds
         last_servo = (uint32_t) millis();
-        baseServo.write(rotation);
-        if (reverse)
+        baseServo.write(rotation); //Go to location
+        if (reverse) //increment location up or down as needed
             rotation--;
         else
             rotation++;
     }
 
-    if (rotation == 140 && reverse == 0) {
+    if (rotation == 70 && reverse == 0) { //once you reach 160 (140 degree sweep), start going backwards
         reverse = 1;
-        if(baseAngle==90)
-            baseAngle=0;
-        baseAngle+=5;
-        topServo.write(baseAngle);
+        if(topAngle==140) //if now speakers are level with the horizontal, start over
+            topAngle=50;
+        topAngle+=5; //otherwise, just incrememnt up angle between grounda and speaker
+        topServo.write(topAngle);
 
     }
-    else if (rotation == 0 && reverse == 1){
+    else if (rotation == 20 && reverse == 1){ //switch back to rotating forward 
         reverse = 0;
     }
 
-    if (!still_pulsed) {
+    if (!still_pulsed) {//got return pusle, get distances and fill distance array
         counter++;
         distance_array[counter%=NUMBER_OF_SAMPLES] = duration *0.034/2;
         calculateDistance();
     }
 
-    uint32_t tmp[NUMBER_OF_SAMPLES];
+
+    uint32_t tmp[NUMBER_OF_SAMPLES]; //Sort distances values and take the median; there's generally a lot of noise and this is the best quick/simple way we had to filter it
     memcpy(&tmp[0], &distance_array[0] , NUMBER_OF_SAMPLES*sizeof(uint32_t));
-    qsort(tmp, NUMBER_OF_SAMPLES, sizeof(uint32_t), uint32Compare);
+    qsort(tmp, NUMBER_OF_SAMPLES, sizeof(uint32_t), uint32Compare); 
     distance = tmp[NUMBER_OF_SAMPLES/2];
-    if ((millis()%120) == 0) {
-        Serial.println(distance);
-        uint16_t distance_x = (uint16_t) (distance * cos(baseAngle * PI / 180));
-        uint16_t distance_y = (uint16_t) (distance * sin(baseAngle * PI / 180));
-        Serial.print("x, y distance: ");
+    int16_t changeDistance = distance_old - distance;
+    if ((millis()%480) == 0) {//only reports every 120 milliseconds
+        distance_old = distance;
+        //if (changeDistance > 75)
+        //    Serial.println("Maybe Detected an Object at distance ");
+        Serial.print("d = ");
+        Serial.print(distance);
+        int16_t distance_floor = (uint16_t) (distance * sin(topAngle * PI / 180)); //see attached PDF for geometric analysis
+        int16_t distance_x = (uint16_t) (distance_floor * cos((rotation + 40) * PI / 180)); //parallel to setup
+        int16_t distance_y = (uint16_t) (distance_floor * sin((rotation + 40) * PI / 180)); //perpendicular to setup
+        Serial.print(", x = ");
         Serial.print(distance_x);
-        Serial.print(", ");
-        Serial.print(distance_y);
-        Serial.print(", ");
-        Serial.println(baseAngle);
+        Serial.print("  y = ");
+        Serial.println(distance_y);
+        //Serial.print(", ");
+        //Serial.println(topAngle);
     }
 }
 
 
-ISR(PCINT2_vect){
-    int value = PIND & (1 << PINB7);
-    //duration = pulseIn(echoPin, LOW); // Reads the echoPin, returns the sound wave travel time in microseconds
-    //distance= duration*0.034/2;
+ISR(PCINT2_vect){ //Checks to see if the speaker is still waiting for a response (pulsed) by comparing the output to a value of 10000000, which only be true if its still waiting
+    int value = PIND & (1 << PIND7);
     if ((value > 0) && !still_pulsed){
-        //Serial.println("test1");
         int_duration = (uint32_t) micros();
         still_pulsed = 1;
     }
     else if ((value == 0) && still_pulsed){
         duration = (uint32_t) (micros() - int_duration);
         still_pulsed = 0;
-        //distance = (uint32_t) (duration * 0.034 / 2);
     }
-    //Serial.println(PIND & (1 << PINB7));
 }
